6457ef322032f7dae0ecfdacd6848ba50f27a92e
quochieng <aiyanarich64@gmail.com>
2024-07-23 09:58:58 +0300

    fix added check for n if n is 0 or n < 3

--- Tribonacci/main.go
+++ Tribonacci/main.go
@@ -11,9 +11,15 @@ func main() {
 // tribbonnacii means triple(adding 3 values to get the next values).
 // fibonnanci means (adding to values to get the next value)
 func Tribonnaci(s [3]float64, n int) []float64 {
+	if n == 0 {
+		return []float64{}
+	}
+	if n < 3 {
+		return s[:n]
+	}
 	value := make([]float64, n)
 	copy(value, s[:])
-	for i := 4; i < len(value); i++ {
+	for i := 3; i < len(value); i++ {
 		value[i] = value[i-1] + value[i-2] + value[i-3]
 	}
 	return value
